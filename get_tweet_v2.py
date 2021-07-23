873dec7d4f5029b4aed56e7d455f8befec669045
TZGyn <65798228+TZGyn@users.noreply.github.com>
2021-07-23 12:58:07 +0800

    Update get_tweet_v2.py

--- get_tweet_v2.py
+++ get_tweet_v2.py
@@ -3,15 +3,6 @@ import os
 from replit import db
 import datetime
 
-def retrieve_last_seen_id(username: str, ids = None):
-    value = db[username].value
-
-    if value == []:
-        store_last_seen_id(ids, username)
-    else:
-        last_seen_id = int(value[-1])
-        return last_seen_id
-
 def retrieve_last_seen_date(username: str, date = None):
     value = db[username]
 
@@ -21,42 +12,12 @@ def retrieve_last_seen_date(username: str, date = None):
         last_seen_date = value
         return last_seen_date
 
-def store_last_seen_id(last_seen_id, username: str):
-    value = db[username]
-    value.append(str(last_seen_id))
-    db[username] = value
-
-    return
-
 def store_last_seen_date(last_seen_date, username: str):
     value = db[username]
     value = str(last_seen_date)
     db[username] = value
 
     return
-
-def get_tweet_id_old(username: str):
-
-    CONSUMER_KEY = os.environ["CONSUMER_KEY"]
-    CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-    ACCESS_KEY = os.environ["ACCESS_KEY"]
-    ACCESS_SECRET = os.environ["ACCESS_SECRET"]
-
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-    API = tweepy.API(auth)
-
-    username = username.strip("@")
-    timelines = API.user_timeline(username, count = 1)
-    if len(timelines) == 0:
-      return None, username
-    retrieve_last_seen_id(username, timelines[0].id)
-
-    if timelines[0].id != retrieve_last_seen_id(username):
-        store_last_seen_id(timelines[0].id, username)
-        return timelines[0].id, username
-    else:
-        return None, username
 
 def get_tweet_id(username: str):
 
@@ -82,4 +43,4 @@ def get_tweet_id(username: str):
         return None, username
 
 if __name__ == "__main__":
-    print(get_tweet_id("TZGyn"))
+    print(get_tweet_id(""))
